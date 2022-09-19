9cac9448556e4417bd821b78d6d06f562c1f548d
anand944 <anandkumarkumbaji07@gmail.com>
2022-09-19 19:43:54 +0530

    newone

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
   features {}
 }
 
-module "ResourceGrop"{
-    source = "./ResourceGrop"
+module "ResourceGroup"{
+    source = "./ResourceGroup"
     base_name = "terraform08"
     location = "east us"
 }
